568c676a9192c1a9ccb6f5ed574dd8a0026c1d17
NeoLied <cini.tristan@gmail.com>
2015-01-29 17:48:08 +0100

    Création des tests pour le module "Users"

--- app/Test/Case/Controller/UsersControllerTest.php
+++ app/Test/Case/Controller/UsersControllerTest.php
@@ -6,9 +6,45 @@ class UserControllerTest extends ControllerTestCase
 {
 	public $fixtures = array('app.user');
 	
-	public function testIndex() 
+	public function test_users_index() 
 	{
 		$result = $this->testAction('/users/index');
+		debug($result);
+	}
+	
+	public function test_users_add()
+	{
+		$result = $this->testAction('/users/add');
+		debug($result);
+	}
+	
+	public function test_users_demande()
+	{
+		$result = $this->testAction('/users/demande');
+		debug($result);
+	}
+	
+	public function test_users_edit()
+	{
+		$result = $this->testAction('/users/edit');
+		debug($result);
+	}
+	
+	public function test_users_offre()
+	{
+		$result = $this->testAction('/users/offre');
+		debug($result);
+	}
+	
+	public function test_users_index()
+	{
+		$result = $this->testAction('/users/validation');
+		debug($result);
+	}
+	
+	public function test_users_index()
+	{
+		$result = $this->testAction('/users/view');
 		debug($result);
 	}
 }
