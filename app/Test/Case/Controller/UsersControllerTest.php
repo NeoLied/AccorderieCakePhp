67815c8a6da987a48c8c623ba81c9f406c38feac
NeoLied <cini.tristan@gmail.com>
2015-02-04 15:12:41 +0100

    Correction des TUs "users"

--- app/Test/Case/Controller/UsersControllerTest.php
+++ app/Test/Case/Controller/UsersControllerTest.php
@@ -18,21 +18,21 @@ class UserControllerTest extends ControllerTestCase
 		debug($result);
 	}
 	
-	public function test_users_demande()
-	{
-		$result = $this->testAction('/users/demande');
-		debug($result);
-	}
-	
 	public function test_users_edit()
 	{
 		$result = $this->testAction('/users/edit/1');
 		debug($result);
 	}
 	
+	public function test_users_login()
+	{
+		$result = $this->testAction('/users/login');
+		debug($result);
+	}
+	
 	public function test_users_offre()
 	{
-		$result = $this->testAction('/users/offre');
+		$result = $this->testAction('/users/view/1');
 		debug($result);
 	}
 }
