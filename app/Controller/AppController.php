70a9608885c1538ef49fe2be10658eb4d7574f1e
Benjiz31 <benjizti31@gmail.com>
2015-02-25 14:34:39 +0100

    Maj visibilité pages
    Accueil visible par tous

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -51,6 +51,6 @@ class AppController extends Controller {
 	}
 
     public function beforeFilter() {
-        $this->Auth->allow('index', 'view');
+        $this->Auth->allow('index', 'view', 'display');
     }
 }
