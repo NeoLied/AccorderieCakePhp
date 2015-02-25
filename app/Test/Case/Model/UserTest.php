0fd4e8735429fbdfb21da8def04e3cc6e6d8a11a
NeoLied <cini.tristan@gmail.com>
2015-02-25 16:56:31 +0100

    Ajout de TU

--- app/Test/Case/Model/UserTest.php
+++ app/Test/Case/Model/UserTest.php
@@ -11,14 +11,19 @@ class UserTestCase extends CakeTestCase
 		$this->User = ClassRegistry::init('User');
 	}
 	
-	public function model_user_isOwnedBy_true()
+	public function testPHPUNIT()
+	{
+		return true;
+	}
+	
+	public function test_model_user_isOwnedBy_true()
 	{
 		$result = $this->User->isOwnedBy(1,1);
 		$expected = true;
 		$this->assertEquals($expected, $result);
 	}
 	
-	public function model_user_isOwnedBy_false()
+	public function test_model_user_isOwnedBy_false()
 	{
 		$result = $this->User->isOwnedBy(1,2);
 		$expected = false;
