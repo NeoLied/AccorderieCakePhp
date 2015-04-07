ebd70d40172de70dd9ef2347c5fae8a84adfbee1
NeoLied <cini.tristan@gmail.com>
2015-04-07 14:50:33 +0200

    Ajout de fixtures USERS

--- app/Test/Fixture/UserFixture.php
+++ app/Test/Fixture/UserFixture.php
@@ -9,10 +9,11 @@ class UserFixture extends CakeTestFixture
 			'username' => array('type' => 'string', 'length' => '100'),
 			'password' => array('type' => 'string', 'length' => '100'),
 			'credit_temps' => array('type' => 'integer'),
+			'offre_de_bienvenue' => array('type' => 'string', 'length' => '3'),
 	);
 	
 	public $records = array(
-			array('id' => 1, 'username' => 'Tristan', 'password' => 'test', 'credit_temps' => 3),
-			array('id' => 2, 'username' => 'aurelien', 'password' => 'aurelien', 'credit_temps' => 3)
+			array('id' => 1, 'username' => 'Tristan', 'password' => 'test', 'credit_temps' => 3,'offre_de_bienvenue' => 'oui'),
+			array('id' => 2, 'username' => 'aurelien', 'password' => 'aurelien', 'credit_temps' => 3,'offre_de_bienvenue' => 'non')
 	);
 }
