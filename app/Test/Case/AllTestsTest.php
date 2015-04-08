b85e787ccc667f90481150a96cad4640127ea2a7
NeoLied <cini.tristan@gmail.com>
2015-04-08 10:42:08 +0200

    test

--- app/Test/Case/AllTestsTest.php
+++ app/Test/Case/AllTestsTest.php
@@ -4,7 +4,7 @@ class AllTestsTest extends CakeTestSuite {
 	public static function suite() {
 		$suite = new CakeTestSuite('All tests');
 		$suite->addTestDirectoryRecursive(TESTS . 'Case/Model');
-		$suite->addTestFile(TESTS . 'Case/Controller' . 'UsersControllerTest.php');
+		$suite->addTestFile(TESTS . 'Case/Controller' . '/UsersControllerTest.php');
 		return $suite;
 	}
 }
