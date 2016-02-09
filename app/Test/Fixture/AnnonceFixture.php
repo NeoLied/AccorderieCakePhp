9b38a7f564e7917ba29635fb794ed698cba53d4d
Linaida <linaida66@gmail.com>
2016-02-09 15:46:47 +0100

    - Ecriture des tests

--- app/Test/Fixture/AnnonceFixture.php
+++ app/Test/Fixture/AnnonceFixture.php
@@ -3,18 +3,23 @@
 class AnnonceFixture extends CakeTestFixture
 {
 	public $useDbConfig = 'test';
-	
+   // public $import ='Annonce';
+
 	public $fields = array(
 			'id' => array('type' => 'integer', 'key' => 'primary'),
+			'type_id' => array('type' => 'integer', 'key' => 'index'),
 			'titre' => array('type' => 'string', 'length' => '50'),
 			'description' => array('type' => 'string', 'length' => '1000'),
 			'date_post' => array('type' => 'date'),
-			'temps_requis' => array('type' => 'integer'),
+			'date_limite' => array('type' => 'date'),
+			'temps_requis' => array('type' => 'integer', 'length' => '100'),
 			'demande' => array('type' => 'integer'),
-			'signalee' => array('type' => 'integer'),
-			'user_id' => array('type' => 'integer'),
-			'id_accepteur' => array('type' => 'integer'),
+			'signalee' => array('type' => 'integer','length' => '4'),
+			'user_id' => array('type' => 'integer','key' => 'index'),
+			'id_accepteur' => array('type' => 'integer','key' => 'index'),
 			'annonceValide' => array('type' => 'string', 'length' => '3'),
+			'statut' => array('type' => 'string', 'length' => '25'),
+			'archive' => array('type' => 'integer', 'length' => '1'),
 	);
 	
 	public $records = array(
@@ -22,25 +27,31 @@ class AnnonceFixture extends CakeTestFixture
 				'id' => 1,
 				'titre' => 'test_annonce',
 				'description' => 'test_desc_annonce',
-				'date_post' => '22/01/2015',
+				'date_post' => '22/01/2016',
+				'date_limite' => '22/06/2016',
 				'temps_requis' => 1,
-				'demande' => 1,
+				'demande' => 0,
 				'signalee' => 0,
 				'user_id' => 2,
 				'id_accepteur' => 3,
 				'annonceValide' => 'oui',
+				'archive' => '0',
+				'type_id' => '5'
 			),
 			array(
 					'id' => 2,
 					'titre' => 'test_annonce_2',
 					'description' => 'test_desc_annonce',
 					'date_post' => '22/01/2015',
+					'date_limite' => '22/06/2016',
 					'temps_requis' => 2,
-					'demande' => 1,
+					'demande' => 0,
 					'signalee' => 0,
-					'user_id' => 1,
+					'user_id' => 29,
 					'id_accepteur' => 0,
 					'annonceValide' => 'non',
+					'archive' => '0',
+					'type_id' => '3'
 			),
 	);
 }
